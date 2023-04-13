8bfd235b321adc083bcd7fc6910c7c9c214b21f2
Tss Chefchaouen <tss.dmsc@gmail.com>
2023-04-13 12:35:13 +0000

    Generate constructor for User class

--- src/main/java/org/demo/example/model/User.java
+++ src/main/java/org/demo/example/model/User.java
@@ -2,4 +2,14 @@ package org.demo.example.model;
 
 public class User {
 	private String firstName;
+
+	/**
+	 * @param firstName
+	 */
+	public User(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	
+	
 }
