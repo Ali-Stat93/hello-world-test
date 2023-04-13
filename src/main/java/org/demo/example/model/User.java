128e5dd664b374aaffda852a26f1d21852be59d0
Tss Chefchaouen <tss.dmsc@gmail.com>
2023-04-13 12:36:52 +0000

    Generate getters and setters for User class

--- src/main/java/org/demo/example/model/User.java
+++ src/main/java/org/demo/example/model/User.java
@@ -9,6 +9,14 @@ public class User {
 	public User(String firstName) {
 		this.firstName = firstName;
 	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 	
 	
 	
